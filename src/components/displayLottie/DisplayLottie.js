4c13bc8672c352d0a718a414cfbcd1c1f5245ed4
Vishal-Nishad <vishal.n1020@gmail.com>
2026-06-06 15:06:43 +0530

    made animation responsive

--- src/components/displayLottie/DisplayLottie.js
+++ src/components/displayLottie/DisplayLottie.js
@@ -5,17 +5,17 @@ import Loading from "../../containers/loading/Loading";
 export default class DisplayLottie extends Component {
   render() {
     const animationData = this.props.animationData;
-    const defaultOptions = {
-      loop: true,
-      autoplay: true,
-      animationData: animationData
-    };
 
     return (
       <Suspense fallback={<Loading />}>
         <Lottie
-          animationData={defaultOptions.animationData}
-          loop={defaultOptions.loop}
+          animationData={animationData}
+          loop={true}
+          style={{
+            width: "100%",
+            maxWidth: "100%",
+            height: "auto"
+          }}
         />
       </Suspense>
     );
