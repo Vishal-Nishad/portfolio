5cb483d7631efe86d20ca78626ba0ad986c01f01
Vishal-Nishad <vishal.n1020@gmail.com>
2026-06-07 21:43:49 +0530

    make skills logo + text style

--- src/portfolio.js
+++ src/portfolio.js
@@ -33,13 +33,13 @@ const greeting = {
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/",
-  facebook: "https://www.facebook.com/",
-  medium: "https://medium.com/",
-  stackoverflow: "https://stackoverflow.com/users/10422806/",
+  github: "https://github.com/Vishal-Nishad",
+  linkedin: "https://www.linkedin.com/in/vishalnishad80/",
+  gmail: "vishalnishadnv@gmail.com",
+  // gitlab: "https://gitlab.com/",
+  // facebook: "https://www.facebook.com/",
+  // medium: "https://medium.com/",
+  // stackoverflow: "https://stackoverflow.com/",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
@@ -399,9 +399,9 @@ const resumeSection = {
 const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
-    "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+    "Discuss an opportunity, collaborate on a project, or just say Hi - my inbox is always open.",
+  number: "+91-8076143667",
+  email_address: "vishalnishadnv@gmail.com"
 };
 
 // Twitter Section
